0aea6b49b96798975d30efc60018d636e767fea2
Calvin Neo <neocezheng@outlook.com>
2023-07-14 10:09:41 +0800

    made some adjustments to fastapi and predict

--- src/predict.py
+++ src/predict.py
@@ -2,9 +2,9 @@ import torch.nn as nn
 import torch
 import pandas as pd
 
-def predict(model, img):
 
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+def predict(model, img):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model.eval()
     model.to(device)
     return torch.argmax(model(img))
